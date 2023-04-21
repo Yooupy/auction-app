ea98739b64107daacd87aa4b0b366dce57a88403
Botirjon Shokirov <botirjon@radcode.co>
2023-04-21 12:53:56 +0200

    build admin dashboard

--- client/src/components/Admin-Panel/index.jsx
+++ client/src/components/Admin-Panel/index.jsx
@@ -1,123 +1,60 @@
-import React, { useContext, useState } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Grid, Paper, Button, Typography } from "@mui/material";
 import styles from "./styles.module.scss";
 import ItemList from "../Items/ItemList";
 import UserList from "../Users/UserList";
 import Wallet from "../Users/Wallet/Wallet";
 import AdminProfile from "./AdminProfile";
 
+const options = [
+  { name: "List of Items", component: <ItemList /> },
+  { name: "List of Users", component: <UserList /> },
+  { name: "Website Wallet", component: <Wallet /> },
+  { name: "Admin Profile", component: <AdminProfile /> },
+];
+
 const Admin = () => {
-  const [activeTab, setActiveTab] = useState("items");
-  const [loginData, setLoginData] = useState({ email: "", password: "" });
-  const { isLoggedIn, user, login, logout } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const [activeOptionIndex, setActiveOptionIndex] = useState(0);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    const { email, password } = loginData;
-    if (email === "admin@mail.com" && password === "TestAdmin1234") {
-      login({ email: "admin@mail.com" });
-    } else {
-      alert("Invalid email or password");
-    }
-    setLoginData({ email: "", password: "" });
+  const handleOptionClick = (optionIndex) => {
+    setActiveOptionIndex(optionIndex);
   };
 
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
-
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
-  };
-
-  const renderActiveTab = () => {
-    switch (activeTab) {
-      case "items":
-        return <ItemList />;
-      case "users":
-        return <UserList />;
-      case "wallet":
-        return <Wallet />;
-      case "profile":
-        return <AdminProfile />;
-      default:
-        return <ItemList />;
-    }
-  };
-
-  if (!isLoggedIn || !user || user.email !== "admin@mail.com") {
+  const renderRightSide = () => {
+    const selectedOption = options[activeOptionIndex];
     return (
-      <div className={styles.admin}>
-        <h2>Please login as an admin to access this page</h2>
-        <form className={styles.loginForm} onSubmit={handleLogin}>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={loginData.email}
-            onChange={(e) =>
-              setLoginData({ ...loginData, email: e.target.value })
-            }
-            required
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            value={loginData.password}
-            onChange={(e) =>
-              setLoginData({ ...loginData, password: e.target.value })
-            }
-            required
-          />
-          <button type="submit">Log In</button>
-        </form>
-      </div>
+      <Paper className={styles.rightSide}>
+        <Typography variant="h4">{selectedOption.name}</Typography>
+        {selectedOption.component}
+      </Paper>
     );
-  }
+  };
 
   return (
-    <div className={styles.admin}>
-      <div className={styles.sidebar}>
-        <div
-          className={`${styles.tab} ${
-            activeTab === "items" ? styles.active : ""
-          }`}
-          onClick={() => handleTabClick("items")}
-        >
-          <span>List of Items</span>
-        </div>
-        <div
-          className={`${styles.tab} ${
-            activeTab === "users" ? styles.active : ""
-          }`}
-          onClick={() => handleTabClick("users")}
-        >
-          <span>List of Users</span>
-        </div>
-        <div
-          className={`${styles.tab} ${
-            activeTab === "wallet" ? styles.active : ""
-          }`}
-          onClick={() => handleTabClick("wallet")}
-        >
-          <span>Website Wallet</span>
-        </div>
-        <div
-          className={`${styles.tab} ${
-            activeTab === "profile" ? styles.active : ""
-          }`}
-          onClick={() => handleTabClick("profile")}
-        >
-          <span>Admin Profile</span>
-        </div>
-        <div className={styles.logout} onClick={handleLogout}>
-          Logout
-        </div>
-      </div>
-      <div className={styles.content}>{renderActiveTab()}</div>
-    </div>
+    <Grid container>
+      <Grid item md={3} sm={4} xs={12}>
+        <Paper className={styles.leftSide}>
+          <Typography variant="h4">Admin Dashboard</Typography>
+          {options.map((option, index) => (
+            <Button
+              key={index}
+              variant="contained"
+              color="primary"
+              className={`${styles.optionButton} ${
+                activeOptionIndex === index ? styles.activeOption : ""
+              }`}
+              onClick={() => handleOptionClick(index)}
+            >
+              {option.name}
+            </Button>
+          ))}
+        </Paper>
+      </Grid>
+      <Grid item md={9} sm={8} xs={12}>
+        {renderRightSide()}
+      </Grid>
+    </Grid>
   );
 };
+
 export default Admin;
--- client/src/components/Users/Wallet/Wallet.jsx
+++ client/src/components/Users/Wallet/Wallet.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+const wallet = {
+  totalBalance: 1000,
+  availableBalance: 750,
+  pendingTransactions: [
+    { id: 1, amount: 50, description: "Transaction 1" },
+    { id: 2, amount: 100, description: "Transaction 2" },
+  ],
+};
+
 const Wallet = () => {
-  return <div>Wallet</div>;
+  return (
+    <div>
+      <h2>Wallet</h2>
+      <p>Total Balance: {wallet.totalBalance}</p>
+      <p>Available Balance: {wallet.availableBalance}</p>
+      <ul>
+        {wallet.pendingTransactions.map((transaction) => (
+          <li key={transaction.id}>
+            <p>Description: {transaction.description}</p>
+            <p>Amount: {transaction.amount}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Wallet;
